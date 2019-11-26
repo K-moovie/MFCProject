504f5a1f8f00760457f2539c581a9602ff30261b
kyha97 <kyha97@naver.com>
2019-11-26 23:40:31 +0900

    add dll

---
+++ WIFDll/CWIF.cpp
@@ -0,0 +1,212 @@
+﻿// CWIF.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "CWIF.h"
+#include "afxdialogex.h"
+
+
+// CWIF 대화 상자
+
+IMPLEMENT_DYNAMIC(CWIF, CDialog)
+
+CWIF::CWIF(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_MAIN, pParent)
+{
+
+}
+
+CWIF::~CWIF()
+{
+}
+
+void CWIF::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CWIF, CDialog)
+	ON_WM_KEYDOWN()
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+// CWIF 메시지 처리기
+
+void CWIF::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+					   // TODO: 여기에 메시지 처리기 코드를 추가합니다.
+					   // 그리기 메시지에 대해서는 CDialog::OnPaint()을(를) 호출하지 마십시오.
+	CString str;
+
+	if (count1 > 100 || count2 > 100) {
+		win = count1 > count2 ? 1 : 2;
+		OnOK();
+	}
+		
+	str.Format(_T("O"));
+	dc.TextOutW(count1, 100,str); // 서버의 좌표
+	dc.TextOutW(count2, 300, str); // 클라이언트의 좌표
+
+
+}
+
+
+
+BOOL CWIF::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	m_Csocket.SetParent(this);
+	m_Ssocket.SetParent(this);
+	count1 = 0;
+	count2 = 0;
+	isRight = FALSE;
+	me = 0;
+	win = 0;
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+void CWIF::OnAccept() {
+	m_Ssocket.Accept(m_Csocket);
+}
+
+void CWIF::OnConnect() {
+
+}
+
+void CWIF::OnClose() {
+	m_Csocket.Close();
+}
+
+void CWIF::OnSend() {
+
+}
+
+void CWIF::OnReceive() {
+	char *pBuf = new char[1025];
+	int iBufSize = 1024;
+	int iRcvd;
+	iRcvd = m_Csocket.Receive(pBuf, iBufSize);
+	if (iRcvd == SOCKET_ERROR) {
+		MessageBox(_T("receiveError"));
+	}
+
+	else {
+		pBuf[iRcvd - 1] = NULL;
+		if (strcmp(pBuf, "count1") == 0)
+		{
+			count1 += 20;
+			Invalidate(TRUE);
+		}
+		else if (strcmp(pBuf, "count2") == 0) {
+			count2 += 20;
+			Invalidate(TRUE);
+		}
+			
+	}
+
+}
+
+
+void CWIF::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CDialog::OnKeyDown(nChar, nRepCnt, nFlags);
+
+	switch (nChar)
+	{
+	case 'p':
+	case 'P':
+		count1 = count2 = 0;
+		Invalidate(TRUE);
+		if (!isMe) { //서버
+			m_Ssocket.Create(4000);
+			m_Ssocket.Listen();
+			break;
+		}
+
+		if (isMe) {
+			m_Csocket.Create();
+			m_Csocket.Connect(m_strName, 4000);
+			break;
+		}
+	}
+	if (!isMe) { // 서버
+		switch (nChar) {
+		case 'a':
+		case 'A':
+			if (!isRight) {
+				me++;
+				isRight = TRUE;
+				break;
+			}
+			else break;
+
+		case 'd':
+		case'D':
+			if (isRight) {
+				me++;
+				if (me == 10) {
+					me = 0;
+					count1 += 20;
+					Invalidate(TRUE);
+					const char * str1("count1");
+					iLen = strlen(str1) + 1;
+					iSent = m_Csocket.Send(LPCTSTR(str1), iLen);
+					if (iSent == SOCKET_ERROR) {
+						MessageBox(_T("send Error"));
+					}
+				}
+				isRight = FALSE;
+				break;
+			}
+			else break;
+		}
+	}
+
+	if (isMe) {  // 클라이언트
+		switch (nChar) {
+		case 'a':
+		case 'A':
+			if (!isRight) {
+				me++;
+				isRight = TRUE;
+				break;
+			}
+			else break;
+
+		case 'd':
+		case'D':
+			if (isRight) {
+				me++;
+				if (me == 10) {
+					me = 0;
+					count2 += 20;
+					Invalidate(TRUE);
+					const char * str1("count2");
+					iLen = strlen(str1) + 1;
+					iSent = m_Csocket.Send(LPCTSTR(str1), iLen);
+					if (iSent == SOCKET_ERROR) {
+						MessageBox(_T("send Error"));
+					}
+				}
+				isRight = FALSE;
+				break;
+			}
+			else break;
+		}
+	}
+	
+
+
+	
+}
+/*
+
+*/
---
+++ WIFDll/resource.h
@@ -0,0 +1,18 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// WIFDll.rc에서 사용되고 있습니다.
+//
+#define IDP_SOCKETS_INIT_FAILED         101
+#define IDD_DIALOG1                     1000
+#define IDD_MAIN                        1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        1002
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           1000
+#endif
+#endif
